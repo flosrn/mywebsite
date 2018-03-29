d34e2608a7d0ca76ff43f1d1518ec250c8d63a69
Flosrn <florian.seran@gmail.com>
2018-03-29 16:37:27 +0200

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
+require('./gulp/tasks/sprites');
+require('./gulp/tasks/scripts');
+require('./gulp/tasks/build'); 
+
+ 
+ 
+
+
+ 
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  entry: {
+    App: "./app/assets/scripts/App.js",
+    Vendor: "./app/assets/scripts/Vendor.js"
+  },
+  output: {
+    path: "./app/temp/scripts",
+    filename: "[name].js"
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel',
+        query: {
+          presets: ['es2015']
+        },
+        test: /\.js$/,
+        exclude: /node_modules/
+      }
+    ]
+  }
+}
